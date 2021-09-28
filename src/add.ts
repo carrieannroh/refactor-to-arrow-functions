89b137b8211b634f6a6c1d3656212e50c8396369
Carrie <carriearoberts95@gmail.com>
2021-09-28 12:05:51 +0100

    my solutions to refactoring into arrow functions

--- src/add.ts
+++ src/add.ts
@@ -5,9 +5,18 @@
  * @param secondNum second number to add
  * @returns total of the two numbers
  */
-function add(firstNum: number, secondNum: number): number {
-  const total = firstNum + secondNum;
-  return total;
-}
+// function add(firstNum: number, secondNum: number): number {
+//   const total = firstNum + secondNum;
+//   return total;
+// }
+
+// const add = (firstNum: number, secondNum: number): number => {
+//   const total = firstNum + secondNum;
+//   return total;
+// }
+
+// const getLastElement = (arr) => arr[element.length - 1];
+
+const add = (firstNum: number, secondNum: number): number => firstNum + secondNum;
 
 export default add;
